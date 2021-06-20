7d5993b7dd77bf11ba632301711ad1a04c1dd808
Sevdanur GENC <sevdanurgenc@gmail.com>
2021-06-20 12:48:47 +0300

    Add files via upload

---
+++ CSharp-OOP-Examples/CSharp-OOP-Examples/CSharp-OOP-Examples/Poly/CokBicimlilik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP_Examples.Poly
+{
+    class CokBicimlilik
+    {
+        public class Uye
+        {
+            public string Ad { get; set; }
+
+            public virtual void CikisYap()
+            {
+                Console.WriteLine(this.Ad + ": Çıkış yaptım.");
+            }
+        }
+
+        public class Admin : Uye
+        {
+            public override void CikisYap()
+            {
+                base.CikisYap();
+
+                Console.WriteLine(this.Ad + ": Çerezleri temizledim.");
+            }
+        }
+    }
+}
---
+++ CSharp-OOP-Examples/CSharp-OOP-Examples/CSharp-OOP-Examples/Polymorphisim/CokBicimlilik.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP_Examples.Polymorphisim
+{
+    class CokBicimlilik
+    {
+        public class Uye {
+            public string Ad { get; set; }
+
+            public virtual void CikisYap() {
+                Console.WriteLine(this.Ad + " kisisi cikis yapti.");
+            } 
+        }
+
+
+        public class Admin : Uye {
+            public override void CikisYap()
+            {
+                //base.CikisYap();
+                Console.WriteLine(this.Ad + " kisisi icin loglar temizlendi.");
+            } 
+        }
+
+
+        //  static void Main(string[] args)
+        //{
+
+        //  CokBicimlilik.Uye uyeOrnegi = new CokBicimlilik.Uye { Ad = "Sevdanur GENC" };
+        //  uyeOrnegi.CikisYap();
+
+        //  CokBicimlilik.Admin adminOrnegi = new CokBicimlilik.Admin { Ad = "Selcuk GENC" };
+        //  adminOrnegi.CikisYap();
+
+        //  Console.ReadKey();
+
+
+        // }
+
+    }
+}
